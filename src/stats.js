27711ded19584855c2ac4165a9766bf34f63b9a7
LukaNikcevic77 <nikcevic.luka77@gmail.com>
2023-04-01 19:33:08 +0200

    Add bar charts

--- src/stats.js
+++ src/stats.js
@@ -1,8 +1,10 @@
 import { Chart, scales } from "chart.js/auto";
 
+
 Chart.defaults.color = '#ffffff';
-Chart.defaults.font.size = 14;
+Chart.defaults.font.size = window.innerWidth / 100;
 Chart.defaults.font.weight = '800';
+//Chart January
 (async function() {
     const data = [
       { week: 1, hours: 25 },
@@ -13,7 +15,7 @@ Chart.defaults.font.weight = '800';
     ];
   
     new Chart(
-      document.getElementById('myfirstchart'),
+      document.getElementById('januarychart'),
       
       {
         type: 'bar',
@@ -23,11 +25,15 @@ Chart.defaults.font.weight = '800';
             {
               
               data: data.map(row => row.hours),
-              backgroundColor: '#f52f2'
+              backgroundColor: '#FFFF'
             }
           ]
         },
         options: {
+
+            responsive: true,
+            maintainAspectRatio: true,
+            aspectRatio: 2,
             plugins: {
                 legend: {
                     display: false,
@@ -40,11 +46,46 @@ Chart.defaults.font.weight = '800';
                 },
             scales: {
                 y: {
+                    position: "right",
+                    
+                  grid: {
+                    color: '#FFFF',
+                    lineWidth: 0.5
+                    
+                  },
+                  border: {
+                    display: false
+                  },
+                  
                     ticks: {
-                        color: '#FFFF00'
+                      
+                      padding: 20,
+
+                      
+                        color: '#FFFF'
                         
-                    }
-                }
+                    },
+                  
+                    
+                },
+                x: {
+                  
+                 
+
+                  grid: {
+
+                    display: false,
+                    color: '#FFFF'
+                  }
+                },
+                yAxes: {
+                  
+                    display: false
+                    
+                    
+                    
+                },
+               
             }
                 
             }
@@ -52,7 +93,184 @@ Chart.defaults.font.weight = '800';
       
     );
   })();
-  
+//Chart February
+(async function() {
+  const data = [
+    { week: 1, hours: 25 },
+    { week: 2, hours: 35 },
+    { week: 3, hours: 60 },
+    { week: 4, hours: 45 }
+   
+  ];
+
+  new Chart(
+    document.getElementById('februarychart'),
+    
+    {
+      type: 'bar',
+      data: {
+        labels: ["Week 1", "Week 2", "Week 3", "Week 4"],
+        datasets: [
+          {
+            
+            data: data.map(row => row.hours),
+            backgroundColor: '#FFFF'
+          }
+        ]
+      },
+      options: {
+
+          responsive: true,
+          maintainAspectRatio: true,
+          aspectRatio: 2,
+          plugins: {
+              legend: {
+                  display: false,
+                  
+                  },
+                  tooltip: {
+                      enabled: false
+                     },
+                 
+              },
+          scales: {
+              y: {
+                  position: "right",
+                  
+                grid: {
+                  color: '#FFFF',
+                  lineWidth: 0.5
+                  
+                },
+                border: {
+                  display: false
+                },
+                
+                  ticks: {
+                    
+                    padding: 20,
+
+                    
+                      color: '#FFFF'
+                      
+                  },
+                
+                  
+              },
+              x: {
+                
+               
+
+                grid: {
+
+                  display: false,
+                  color: '#FFFF'
+                }
+              },
+              yAxes: {
+                
+                  display: false
+                  
+                  
+                  
+              },
+             
+          }
+              
+          }
+      }
+    
+  );
+})();
+//Chart March
+(async function() {
+  const data = [
+    { week: 1, hours: 25 },
+    { week: 2, hours: 35 },
+    { week: 3, hours: 60 },
+    { week: 4, hours: 45 }
+   
+  ];
+
+  new Chart(
+    document.getElementById('marchchart'),
+    
+    {
+      type: 'bar',
+      data: {
+        labels: ["Week 1", "Week 2", "Week 3", "Week 4"],
+        datasets: [
+          {
+            
+            data: data.map(row => row.hours),
+            backgroundColor: '#FFFF'
+          }
+        ]
+      },
+      options: {
+
+          responsive: true,
+          maintainAspectRatio: true,
+          aspectRatio: 2,
+          plugins: {
+              legend: {
+                  display: false,
+                  
+                  },
+                  tooltip: {
+                      enabled: false
+                     },
+                 
+              },
+          scales: {
+              y: {
+                  position: "right",
+                  
+                grid: {
+                  color: '#FFFF',
+                  lineWidth: 0.5
+                  
+                },
+                border: {
+                  display: false
+                },
+                
+                  ticks: {
+                    
+                    padding: 20,
+
+                    
+                      color: '#FFFF'
+                      
+                  },
+                
+                  
+              },
+              x: {
+                
+               
+
+                grid: {
+
+                  display: false,
+                  color: '#FFFF'
+                }
+              },
+              yAxes: {
+                
+                  display: false
+                  
+                  
+                  
+              },
+             
+          }
+              
+          }
+      }
+    
+  );
+})();
    
   
    
